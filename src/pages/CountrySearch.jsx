5ef9b4cf147b814f5818bc02a5f5b920fc1450fd
AnnVynnychenkoa <avinnichenko93@gmail.com>
2023-08-20 12:15:21 +0300

    search-country

--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -2,16 +2,44 @@ import {
   Container,
   SearchForm,
   Section,
-  Heading,
   Loader,
   CountryList,
 } from 'components';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchByRegion } from 'service/country-service';
 
 export const CountrySearch = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [countries, setCountries] = useState([]);
+  const region = searchParams.get('region');
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    if (!region) {
+      return;
+    }
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const resultData = await fetchByRegion(region);
+        setCountries(resultData);
+      } catch (error) {
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [region]);
+
+  const handleSubmit = region => {
+    setSearchParams({ region });
+  };
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+        <SearchForm handleSubmit={handleSubmit} />
+        <CountryList countries={countries} />
+        {loading && <Loader />}
       </Container>
     </Section>
   );
