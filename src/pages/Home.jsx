9b78786ecc407cc1dda4a5bf0986c045940a07e0
Kostyantyn <kosta.rusin@gmail.com>
2023-08-20 10:27:39 +0200

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,24 @@
-import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { Container, CountryList, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/country-service';
 
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getCountries()
+      .then(data => setCountries(data))
+      .catch(e => console.log(e))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <Section>
       <Container>
-        <h2>Home</h2>
+        {loading && <Loader />}
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
