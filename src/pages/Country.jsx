83b257d214be9d5927a9f6514e4946ac5a1d511e
Kostyantyn <kosta.rusin@gmail.com>
2023-08-20 11:55:15 +0200

    counrtyInfo Go back

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,29 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
+import { useEffect, useState } from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/country-service';
 
 export const Country = () => {
+  const { countryId } = useParams();
+  const location = useLocation();
+  const [country, setCountry] = useState({});
+  const [isLoading, setIsloading] = useState(false);
+
+  useEffect(() => {
+    if (!countryId) return;
+    setIsloading(true);
+    fetchCountry(countryId)
+      .then(data => setCountry(data))
+      .catch(error => console.log(error))
+      .finally(setIsloading(false));
+  }, [countryId]);
+
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        <Link to={location.state?.from ?? '/'}>Go back</Link>
+        {isLoading && <Loader />}
+        <CountryInfo {...country} />
       </Container>
     </Section>
   );
